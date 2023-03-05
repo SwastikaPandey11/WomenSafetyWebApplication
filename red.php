7c98a6939deb08cb4d14222c6becde56b96e3d80
SwastikaPandey11 <89079668+SwastikaPandey11@users.noreply.github.com>
2023-03-05 16:25:17 +0530

    Update red.php

--- red.php
+++ red.php
@@ -7,7 +7,12 @@ $dbname="women";
 $conn=mysqli_connect($servername,$username,$passsword,$dbname);
 $us=$_GET['id'];
 $sql="INSERT INTO location(lat,lon,acc,User_ID,red,green,orange) values(0,0,0,'$us',1,0,0)";
+$phone= "SELECT Phone_no from family where User_ID='$us'";
 mysqli_query($conn,$sql);
+$retval=mysqli_query($conn,$phone);
+//while($row = mysqli_fetch_array($retval, MYSQL_ASSOC)) {
+  //echo '<script type="text/javascript">alert("'.$row['Phone_no'].'");</script>';
+//}
 
 ?>
 <!DOCTYPE html>
@@ -23,29 +28,32 @@ mysqli_query($conn,$sql);
 <link href="https://fonts.googleapis.com/css2?family=Modern+Antiqua&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
 
 <style>
-body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
-body {font-size:16px;background-color: #CEDCF6;}
-.w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.9;cursor:pointer;border-radius:30px}
+h1,h2,h3,h4,h5 {font-family: "Playfair Display", sans-serif}
+body {font-size:16px;background-color:white;}
+.w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.9;cursor:pointer;border-radius:30px;background-color:#efd0cc}
 .w3-half img:hover{opacity:1}
 #hd
 {
- font-family:'Amatic SC';
- font-size: 70px;
+ font-family:'Playfair Display';
+ font-size: 40px;
 
 }
 #main
 {
-font-family: 'Chewy';
+font-family: 'Playfair Display';
 }
 #mySideBar
 {
     box-shadow: 5px;
 }
 .success {
-  background-color:cornflowerblue;
-  border-left: 6px solid blue;
+  background-color:#efd0cc;
+  border-left: 6px solid #E8B9AB;
 }
 
 .mainn
@@ -77,19 +85,45 @@ font-family: 'Chewy';
             width: 700px;
             height: 500px;
         }
+        #call{
+          position:Relative;
+            top:-200px;
+        }
+        
 </style>
 </head>
 <body>
-
+<!--Add correct page-->
+<script src="//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.1/annyang.min.js"></script>
+  <script>
+  if (annyang) {
+    // Let's define a command.
+    const commands = {
+      'home': () => {location.href="upload.php?username=<?php 
+                            echo $us;                        
+                            ?>"}
+    };
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+  
+    // Start listening.
+    annyang.start();
+  }
+  </script>
 <!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-blue w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+<nav class="w3-sidebar  w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:280px;font-weight:bold; background-color:#efd0cc;font-family:Playfair Display" id="mySidebar"><br>
   <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">CLOSE MENU</a>
   <div class="w3-container">
-    <h2 class="w3" id="hd"><b>SURAKSHA SAATHI</b></h2>
+    <h2 class="w3" id="hd"><b>Suraksha Saathi</b></h2>
   </div>
   <div class="w3-bar-block">
+    <br>
+    <br>
+    <br>
     <a href="loginpage.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">DIFFERENT MEMBER?</a> 
-    <a href="right.html" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">KNOW ABOUT YOUR RIGHTS</a> 
+    <a href="upload.php?username=<?php 
+                            echo $us;                        
+                            ?>" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a>
     <hr>
     <hr>
    
@@ -107,8 +141,8 @@ font-family: 'Chewy';
 
 <div class="w3-main" style="margin-left:340px;margin-right:40px">
     <div class="w3-container" style="margin-top:60px" id="showcase">
-        <h1 class="w3-jumbo" id="main"><b>RED ALERT!!</b></h1>
-        <h2>DON'T WORRY, WE ARE WITH YOU</h2>
+        <h1 class="w3-jumbo" id="main"><b>Red Alert</b></h1>
+        <h2 style="font-family: 'Dancing Script'">Don't Worry,We are with you</h2>
         <p>A call to the women help centre is being done along with sharing your location.An alert message and location will also be send to your Registered Number!! </p>
     </div>
     <div class="w3-row-padding">
@@ -116,17 +150,20 @@ font-family: 'Chewy';
         <div class="w3-half Container">
 
           <div id="map"></div>
-          <p style="float:right;position:relative;top:-300px;left:500px"><a href="tel:9315913617"><img src="loc2.jpg" style="width:300px; border-radius:0px;"></a></p>
-          <p style="float:right;position:relative;top:-250px;left:500px"><a href="tel:<?php
+          <div id="call">
+          <p style="float:right;position:relative;top:-300px;left:500px"><a href="tel:9315813617"><img src="loc2.jpg" style="width:300px; border-radius:0px;box-shadow: 0px 1px 20px 10px rgba(0, 0, 0, 0.2);"></a></p>
+          <p style="float:right;position:relative;top:-250px;left:500px"><a href="tel:9087823017">
+          <!-- <?php
                                                                              
                                                                              $sql="SELECT Phone_no from family where User_ID='$us'";
                                                                              echo $sql;
-                                                                             ?>
-          "><img src="loc2.jpg" style="width:300px; border-radius:0px;"></a></p>
+                                                                             ?> -->
+          <!-- "> -->
+          <img src="loc2.jpg" style="width:300px; box-shadow: 0px 1px 20px 10px rgba(0, 0, 0, 0.2);"></a></p>
         </div>
-        
+      </div>
     </div>
-    <div class="success" style="position:relative; top:-90px;height:80px;padding:10px;">
+    <div class="success" style="position:relative; top:-300px;height:80px;padding:10px;">
         <p><strong>Location and alert message have been send</strong></p>
       </div>
     
